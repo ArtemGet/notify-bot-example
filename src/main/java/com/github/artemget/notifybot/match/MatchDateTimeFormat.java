f01a81341ceb3ce7a768f20347d7951ffc3cdd4b
Artem Getmanskii <re910@yandex.ru>
2024-05-02 22:57:21 +0300

    implement MatchDateTimeFormat

--- src/main/java/com/github/artemget/notifybot/match/MatchDateTimeFormat.java
+++ src/main/java/com/github/artemget/notifybot/match/MatchDateTimeFormat.java
@@ -2,13 +2,35 @@ package com.github.artemget.notifybot.match;
 
 import com.github.artemget.teleroute.match.Match;
 import com.github.artemget.teleroute.update.Wrap;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import lombok.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Value
 @Component
 public class MatchDateTimeFormat implements Match<Update> {
+    DateTimeFormatter formatter;
+
     @Override
     public Boolean match(final Wrap<Update> wrap) {
-        throw new UnsupportedOperationException("#match()");
+        return wrap.text()
+            .map(this::validate)
+            .orElse(false);
+    }
+
+    private boolean validate(final String text) {
+        boolean valid = false;
+        try {
+            final String[] split = text.split(" ");
+            LocalDateTime.parse(
+                split[1] + ' ' + split[2],
+                this.formatter);
+            valid = true;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return valid;
     }
 }
